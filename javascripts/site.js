6676d725dd12fcabcfccf849fec331477f1b8ca1
Cam Burrows <cameron@speeddigital.com>
2020-06-22 17:27:43 -0400

    build

---
+++ javascripts/site.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+  //nav btns
+  $(document).on('click', '.work-btn', function(){
+    $('.content-pane.active-pane').toggleClass('active-pane closed-pane')
+    $('.work-pane').toggleClass('active-pane closed-pane')
+    $('.about-container').removeClass('about-delay')
+  })
+  
+  $(document).on('click', '.about-btn', function(){
+    $('.content-pane.active-pane').toggleClass('active-pane closed-pane')
+    $('.about-pane').toggleClass('active-pane closed-pane')
+    $('.about-container').addClass('about-delay')
+  })
+  
+  $(document).on('click', '.home-btn', function(){
+    $('.content-pane.active-pane').toggleClass('active-pane closed-pane')
+    $('.landing-pane').toggleClass('active-pane closed-pane')
+    $('.about-container').removeClass('about-delay')
+  })
+  
+
+  //gallery old
+  $(".extend-gallery-button").on('click', function(){
+    if ($("#thumbnails").hasClass('open-gallery')){
+      $("#thumbnails").removeClass('open-gallery');
+      $(".extend-gallery-button").html('More Images');
+    }
+    else {
+      $("#thumbnails").addClass('open-gallery');
+      $(".extend-gallery-button").html('Less Images');
+    }
+  })
+})
